f853b6550c8df42046a890e4ef3718def858e679
srushtimane27 <srushtimane31@gmail.com>
2022-07-26 22:24:23 +0530

    Test cases for first name

--- src/main/java/com/Bridgelabz/UserValidator.java
+++ src/main/java/com/Bridgelabz/UserValidator.java
@@ -1,2 +1,12 @@
-package com.Bridgelabz;public class UserValidator {
+package com.Bridgelabz;
+import java.util.regex.Pattern;
+
+public class UserValidator {
+    private static final String FIRST_NAME_PATTERN = "^[A-Z] [a-z] {2,} $";
+
+    public boolean validateFirstName(String fname) {
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        return pattern.matcher(fname).matches();
+    }
+
 }
--- src/test/java/com/Bridgelabz/UserValidatorTest.java
+++ src/test/java/com/Bridgelabz/UserValidatorTest.java
@@ -1,2 +1,27 @@
-package com.Bridgelabz;public class UserValidatorTest {
+package com.Bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserValidatorTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("Srushti");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("Sr");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void givenFirstName_WhenFirstLetterCapital_ShouldReturnTrue(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("Narayan");
+        Assertions.assertTrue(result);
+    }
+
+
 }
