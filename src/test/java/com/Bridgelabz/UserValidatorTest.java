cdaa8a39e84a4aef7f51ad8463c923dcf11a8301
srushtimane27 <srushtimane31@gmail.com>
2022-07-26 23:00:20 +0530

    Use cases for last name

--- src/test/java/com/Bridgelabz/UserValidatorTest.java
+++ src/test/java/com/Bridgelabz/UserValidatorTest.java
@@ -1,9 +1,13 @@
+//Test Cases For User Registration
+
 package com.Bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserValidatorTest {
+
+    //Test Cases For First Name
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue(){
         UserValidator validator = new UserValidator();
@@ -21,6 +25,19 @@ public class UserValidatorTest {
         UserValidator validator = new UserValidator();
         boolean result = validator.validateFirstName("Narayan");
         Assertions.assertTrue(result);
+    }
+    //Test Cases For Last Name
+    @Test
+    public void givenLastName_WhenLastLetterCapital_ShouldReturnTrue(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Mane");
+        Assertions.assertFalse(result);
+    }
+    @Test
+    public void givenLastName_WhenLastNameHasMinThreeCharacters_ShouldReturnTrue(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Ma");
+        Assertions.assertFalse(result);
     }
 
 
