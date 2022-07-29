ad46e7ec94df9c61920781292e68bae189030acf
srushtimane27 <107099158+srushtimane27@users.noreply.github.com>
2022-07-29 17:14:12 +0530

    Update UserValidatorTest.java

--- src/test/java/com/Bridgelabz/UserValidatorTest.java
+++ src/test/java/com/Bridgelabz/UserValidatorTest.java
@@ -1,18 +1,34 @@
 //Test Cases For User Registration
-
 package com.Bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class UserValidatorTest {
 
-    //Test Cases For First Name
+    /**
+     * Test Cases For First Name
+     */
+    
     @Test
-    public void givenFirstName_WhenStartsWithCapsAndHasMinimumThreeCharacters_ShouldReturnTrue(){
+    public void givenFirstName_WhenStartsWithCapsAndHasMinThreeLetters_ShouldReturnTrue(){
         UserValidator validator = new UserValidator();
-        boolean result = validator.validateFirstName("Alka");
+        boolean result = validator.validateFirstName("Srushti");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("Sr");
         Assertions.assertFalse(result);
     }
+    @Test
+    public void givenFirstName_WhenStartsWithSmallLetter_ShouldReturnFalse(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("srushti");
+        Assertions.assertFalse(result);
+    }
+
 
 }
