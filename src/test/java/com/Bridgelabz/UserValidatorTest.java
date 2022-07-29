816f795774d321f15e0631ec6ef625f10a7d5665
srushtimane27 <107099158+srushtimane27@users.noreply.github.com>
2022-07-29 17:19:10 +0530

    Update UserValidatorTest.java

--- src/test/java/com/Bridgelabz/UserValidatorTest.java
+++ src/test/java/com/Bridgelabz/UserValidatorTest.java
@@ -5,24 +5,56 @@ package com.Bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class UserValidatorTest {
 
-    //Test Cases For First Name
+    /**
+     * Test Cases For First Name
+     */
+    
+    
     @Test
-    public void givenFirstName_WhenStartsWithCapsAndHasMinimumThreeCharacters_ShouldReturnTrue(){
+    public void givenFirstName_WhenStartsWithCapsAndHasMinThreeLetters_ShouldReturnTrue(){
         UserValidator validator = new UserValidator();
-        boolean result = validator.validateFirstName("Alka");
+        boolean result = validator.validateFirstName("Srushti");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("Sr");
         Assertions.assertFalse(result);
     }
-
-    //Test Cases For Last Name
     @Test
-    public void givenLastName_WhenStartsWithCapsAndHasMinimumThreeCharacters_ShouldReturnTrue(){
+    public void givenFirstName_WhenStartsWithSmallLetter_ShouldReturnFalse(){
         UserValidator validator = new UserValidator();
-        boolean result = validator.validateLastName("Mane");
+        boolean result = validator.validateFirstName("srushti");
         Assertions.assertFalse(result);
     }
+    
 
+    /**
+     * Test Case For Last Name
+     */
+    
 
+    @Test
+    public void givenLastName_WhenLastLetterCapitalAndHasMinThreeLetters_ShouldReturnTrue(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Abcdef");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void givenLastName_WhenShort_ShouldReturnFalse(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Ab");
+        Assertions.assertFalse(result);
+    }
+    @Test
+    public void givenLastName_WhenStartsWithSmallLetters_ShouldReturnFalse(){
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("abcd");
+        Assertions.assertFalse(result);
+    }
 
 }
