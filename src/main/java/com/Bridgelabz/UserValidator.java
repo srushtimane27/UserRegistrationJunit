2aaae0cc5a8b8399e94ea6036aecc5a892ab55c7
srushtimane27 <107099158+srushtimane27@users.noreply.github.com>
2022-07-29 17:20:38 +0530

    Update UserValidator.java

--- src/main/java/com/Bridgelabz/UserValidator.java
+++ src/main/java/com/Bridgelabz/UserValidator.java
@@ -2,16 +2,27 @@ package com.Bridgelabz;
 import java.util.regex.Pattern;
 
 public class UserValidator {
-    private static final String FIRST_NAME_PATTERN = "^[A-Z] {1} [a-z] {2,} $";
-    private static final String LAST_NAME_PATTERN = "^[A-Z] {1} [A-Z] {2,} $";
+    private static final String FIRST_NAME_REGEX = "^[A-Z][a-z]{2,}";
+    static boolean isFirstNameValid;
 
-    public boolean validateFirstName(String fname) {
-        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
-        return pattern.matcher(fname).matches();
+    public boolean validateFirstName(String firstName) {
+        isFirstNameValid = Pattern.matches(FIRST_NAME_REGEX, firstName);
+        if (!isFirstNameValid) {
+            System.out.println("First Name is invalid...Please enter proper first name");
+        }
+        return Pattern.matches(FIRST_NAME_REGEX, firstName);
     }
-    public boolean validateLastName(String lname) {
-        Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
-        return pattern.matcher(lname).matches();
+
+    private static final String LAST_NAME_REGEX = "^[A-Z][a-z]{2,}";
+    static boolean isLastNameValid;
+
+    public boolean validateLastName(String lastName) {
+        isLastNameValid= Pattern.matches(LAST_NAME_REGEX, lastName);
+        if(!isLastNameValid) {
+            System.out.println("Last Name is invalid...Please enter proper first name");
+        }
+        return Pattern.matches(LAST_NAME_REGEX, lastName);
     }
+
 
 }
